f2763eca2d02906a2412ae6d774b46c3befdc729
Sera Naoto <sera@4am.jp>
2025-12-10 22:10:01 +0900

    Add export lodash

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+git push --tags
+npm run build
+
+COMMENT="$1"
+
+if [ -z "${COMMENT}" ]; then
+  echo "コメントは必須です"
+  exit 1
+fi
+
+git add -A
+git commit -m "${COMMENT}"
+git push
+
+npm version patch
+git push --tags
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import {LoDashStatic, size, isNil, debounce, isEqual} from "lodash";
+import {LoDashStatic, size, isNil, debounce, isEqual, last, first} from "lodash";
 
 declare global {
     interface Array<T> {
@@ -65,6 +65,8 @@ interface AnsukoType extends LoDashStatic {
     isNil: typeof isNil
     debounce: typeof debounce
     isEqual: typeof isEqual
+    first: typeof first,
+    last: typeof last
 }
 
 declare const _default: AnsukoType
